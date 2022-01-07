e88865da443a92f2f424d48561ea47e66a87b581
Leiyi548 <1424630446@qq.com>
2022-01-07 19:38:13 +0800

    删除没用的信息

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -5,54 +5,9 @@ M.config = function()
 		{ "TermOpen", "term://*", "lua require('user.keybindings').set_terminal_keymaps()" },
 		{ "TermOpen", "term://*", "nnoremap <silent> <buffer> q :q<CR>" },
 		-- quickfix
-		{ "FileType", "quickfix", "nnoremap <silent> <buffer> q :q<CR>" },
+		{ "FileType", "quickfix", "nnoremap <silent> <buffer> q :q<cr>" },
 		-- dashboard
 		-- { "FileType", "alpha", "nnoremap <silent> <buffer> q :q<CR>" },
-		{
-			"FileType",
-			"alpha",
-			"setlocal nocursorline noswapfile synmaxcol& signcolumn=no norelativenumber nocursorcolumn nospell  nolist  nonumber bufhidden=wipe colorcolumn= foldcolumn=0 matchpairs= ",
-		},
-		-- { "FileType", "alpha", "set laststatus=0" },
-		-- {
-		-- 	"FileType",
-		-- 	"alpha",
-		-- 	"set showtabline=0 | autocmd BufLeave <buffer> set showtabline=" .. vim.opt.showtabline._value,
-		-- },
-		-- python
-		-- { "FileType", "python", "inoremap <silent> <buffer> ,, <++>"},
-		-- { "FileType", "python", "inoremap <silent> <buffer> ,f <Esc>/<++><CR>:nohlsearch<CR>c4l"},
-		-- g++  -std=c++17 -g -Wall "$(VIM_FILEPATH)" -o "$(VIM_FILEDIR)/a.out" && "$(VIM_FILEDIR)/a.out"
-		--  $(VIM_FILEPATH)    # 当前 buffer 的文件名全路径
-		--  $(VIM_FILEDIR)     # 当前 buffer 的文件所在路径
-		-- {
-		-- 	"Filetype",
-		-- 	"c,cpp",
-		-- 	"nnoremap <leader>rr <cmd>lua require('lvim.core.terminal')._exec_toggle({cmd='g++ -std=c++17 -g -Wall "
-		-- 		.. vim.fn.expand("%")
-		-- 		.. "',count=2,direction='float'})<CR>",
-		-- },
-		{
-			"Filetype",
-			"python",
-			"nnoremap <leader>rr <cmd>lua require('lvim.core.terminal')._exec_toggle({cmd='python " .. vim.fn.expand(
-				"%"
-			) .. "',count=2,direction='float'})<CR>",
-		},
-		{
-			"Filetype",
-			"lua",
-			"nnoremap <leader>rr <cmd>lua require('lvim.core.terminal')._exec_toggle({cmd='lua "
-				.. vim.fn.expand("%")
-				.. "',count=2,direction='float'})<CR>",
-		},
-		{
-			"Filetype",
-			"typescript",
-			"nnoremap <leader>rr <cmd>lua require('lvim.core.terminal')._exec_toggle({cmd='ts-node " .. vim.fn.expand(
-				"%"
-			) .. "',count=2,direction='float'})<CR>",
-		},
 	}
 end
 if lvim.builtin.fancy_number.active then
