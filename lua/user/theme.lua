19d5e614ae1648f6126afe25c80b95ce71eac920
Leiyi548 <1424630446@qq.com>
2022-02-03 23:50:01 +0800

    make onedarker colorscheme comment italic

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -182,3 +182,46 @@ vim.gruvbox_italicize_comments = 1
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━━━━━❰ end gruvbox configs ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━❰ onedarkpro configs ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+local onedarkpro = require("onedarkpro")
+onedarkpro.setup({
+	-- Theme can be overwritten with 'onedark' or 'onelight' as a string!
+	theme = function()
+		if vim.o.background == "dark" then
+			return "onedark"
+		else
+			return "onelight"
+		end
+	end,
+	colors = {}, -- Override default colors. Can specify colors for "onelight" or "onedark" themes by passing in a table
+	hlgroups = {}, -- Override default highlight groups
+	plugins = { -- Override which plugins highlight groups are loaded
+		native_lsp = true,
+		polygot = true,
+		treesitter = true,
+		-- Others omitted for brevity
+	},
+	styles = {
+		strings = "NONE", -- Style that is applied to strings
+		comments = "italic", -- Style that is applied to comments
+		keywords = "NONE", -- Style that is applied to keywords
+		functions = "NONE", -- Style that is applied to functions
+		variables = "NONE", -- Style that is applied to variables
+	},
+	options = {
+		bold = true, -- Use the themes opinionated bold styles?
+		italic = true, -- Use the themes opinionated italic styles?
+		underline = true, -- Use the themes opinionated underline styles?
+		undercurl = true, -- Use the themes opinionated undercurl styles?
+		cursorline = false, -- Use cursorline highlighting?
+		transparency = false, -- Use a transparent background?
+		terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
+		window_unfocussed_color = false, -- When the window is out of focus, change the normal background?
+	},
+})
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━❰ end onedarkpro configs ❱━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
